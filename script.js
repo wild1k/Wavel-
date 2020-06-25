716449005b4a651e393cad28777ff9034e9f6de0
Ariel <arielbs1991@gmail.com>
2020-06-25 10:39:19 -0700

    fixed image link errors. added some classes and ids to html file

--- script.js
+++ script.js
@@ -36,38 +36,52 @@ function openWeatherGet(citySearch) {
                 //grabbing main weather description to narrow down icon variables
                 var weatherObject = response.daily[i].weather[0].main;
                 //if we want more detailed weather for a broader range of icons by id we can use:
-
+                console.log(weatherObject);
                 //var weatherObject = response.daily[i].weather[0].id
 
                 //stringifying our weatherObject so we can use it as a working variable
-                var weatherString = JSON.stringify(weatherObject);
+                
                 var forecast = {
                     date: convertedDate,
                     temp: response.daily[i].temp.day,
                     humidity: response.daily[i].humidity,
-                    //using toLowerCase on weatherString for ease of image linking
-                    description: weatherString.toLowerCase(),
+                    //using toLowerCase on weatherObject for ease of image linking
+                    description: weatherObject.toLowerCase(),
                     card: $(".cardRow")
                 }
-                console.log(forecast.description);
+               
+                // console.log(forecast.description);
                 //TODO: make weather icon link work
                 //still need working weather icon link, and alt is not displaying for some reason (syntax error?)
                 //html syntax of our forecast cards
+                var sourceString = `weather-icons/${forecast.description}.png`
+                // console.log(sourceString);
+                console.log(forecast);
                 var details = `<div class="col m2 s6 push-m1">
-                 <div class="card small">
-                         <div class="card-image">
-                           <img alt="${forecast.description}" src="weather-icons\" + ${forecast.description} + ".png">
-                         </div>
-                         <div class="card-content">
-                           <p>${forecast.date}</p>
-                           <p>Temp: ${forecast.temp}°F</p>
-                           <p>Humidity: ${forecast.humidity}%</p>
-                         </div>
-                         </div>`
-
+                <div class="card small">
+                        <div class="card-image">
+                          <img src= ${sourceString}>
+                        </div>
+                        <div class="card-content">
+                          <p>${forecast.date}</p>
+                          <p>Temp: ${forecast.temp}°F</p>
+                          <p>Humidity: ${forecast.humidity}%</p>
+                        </div>
+                        </div>`
                 //appending forecast details onto cards for five day forecast
                 forecast.card.append(details);
             };
         });
     });
 }
+
+// var divCol = $(`<div class="col m2 s6 push-m1"></div>`)
+// var divCard = $(`<div class="card small"></div>`)
+// var divCardImage =$(`<div class="card-image"></div>`)
+// var imgSrc = $(`<img src="weather-icons/${forecast.description}.png">`)
+//         <div class="card-content">
+//            <p>${forecast.date}</p>
+//            <p>Temp: ${forecast.temp}°F</p>
+//            <p>Humidity: ${forecast.humidity}%</p>
+
+//          `)
